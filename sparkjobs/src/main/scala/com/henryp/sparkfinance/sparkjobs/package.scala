8afba0f6d8f273727fab4de19d80c188f20e90d1
PhillHenry <phillhenry@gmail.com>
2015-11-07 15:54:00 +0000

    More generic.

--- sparkjobs/src/main/scala/com/henryp/sparkfinance/sparkjobs/package.scala
+++ sparkjobs/src/main/scala/com/henryp/sparkfinance/sparkjobs/package.scala
@@ -8,8 +8,9 @@ import scala.reflect.ClassTag
 
 package object sparkjobs extends Logging {
 
-  type TickerDate = String
-
+  /**
+   * @param all An RDD of key to content. Typically, filename to file content if loaded via SparkContext.wholeTextFiles.
+   */
   def aggregate[T: ClassTag](all:        RDD[(String, String)],
                              isNotMeta:  String => Boolean,
                              toDomain:   String => T ): RDD[T] = {
@@ -20,13 +21,13 @@ package object sparkjobs extends Logging {
     all.flatMap { case(ticker, text) => toRDD(ticker, text) }
   }
 
-  def dateAndPriceFor(raw:                RDD[String],
-                      isNotMeta:          String => Boolean,
-                      lineToDateAndPrice: String => (TickerDate, Double)): RDD[(TickerDate, Double)] =
-    raw.filter(isNotMeta(_)).map(lineToDateAndPrice(_))
+  def data[T: ClassTag](raw:        RDD[String],
+                        isNotMeta:  String => Boolean,
+                        toDomain:   String => T): RDD[T] =
+    raw.filter(isNotMeta(_)).map(toDomain(_))
 
-  def pearsonCorrelationValue[K](keyVal1: RDD[(K, Double)], keyVal2: RDD[(K, Double)]): Double = {
-    val joined: RDD[(K, (Double, Double))] = keyVal1.join(keyVal2)
+  def pearsonCorrelationValue(keyVal1: RDD[(String, Double)], keyVal2: RDD[(String, Double)]): Double = {
+    val joined: RDD[(String, (Double, Double))] = keyVal1.join(keyVal2)
 
     val series1  = joined map { case(key, forKey) => forKey._1 }
     val series2  = joined map { case(key, forKey) => forKey._2 }
