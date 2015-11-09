153901f03c8a37bff71a8c1da1a1cd20eb4e2286
PhillHenry <phillhenry@gmail.com>
2015-11-09 07:11:21 +0000

    Working app.

--- logging/src/main/scala/com/henryp/sparkfinance/logging/Logging.scala
+++ logging/src/main/scala/com/henryp/sparkfinance/logging/Logging.scala
@@ -4,5 +4,7 @@ trait Logging {
 
   def debug(msg: => String): Unit = println(msg)
 
-  def error(msg: => String): Unit = println(msg) // do some proper logging
+  def error(msg: => String): Unit = debug(msg) // do some proper logging
+
+  def info(msg: => String): Unit = debug(msg) // TODO proper logging
 }
--- sparkjobs/src/main/scala/com/henryp/sparkfinance/sparkjobs/yahoo/StockCorrelation.scala
+++ sparkjobs/src/main/scala/com/henryp/sparkfinance/sparkjobs/yahoo/StockCorrelation.scala
@@ -7,10 +7,19 @@ import com.henryp.sparkfinance.sparkjobs._
 
 import scala.annotation.tailrec
 
-case class StockCorrelationConfig(directory: String = "./target",
-                                  sparkUrl: String = Spark.localMaster,
-                                  tickers: Seq[String] = List[String]())
+case class StockCorrelationConfig(directory: String     = "./target",
+                                  sparkUrl: String      = Spark.localMaster,
+                                  tickers: Seq[String]  = List[String](),
+                                  jars: Seq[String]     = List[String]() )
 
+/**
+ * Run with arguments like:
+ *
+ * -d /home/henryp/Documents/Finance/Historical/
+ * -t HSBA,BARC
+ * -s spark://192.168.1.9:7077
+ * -j /home/henryp/Code/Scala/MyCode/SparkFinance/sparkjobs/target/sparkjobs-1.0-SNAPSHOT.jar,/home/henryp/Code/Scala/MyCode/SparkFinance/logging/target/logging-1.0-SNAPSHOT.jar,/home/henryp/Code/Scala/MyCode/SparkFinance/feeds/target/feeds-1.0-SNAPSHOT.jar
+ */
 object StockCorrelation extends Logging {
 
   def parseArgs(args: Array[String]): Option[StockCorrelationConfig] = {
@@ -20,6 +29,9 @@ object StockCorrelation extends Logging {
       opt[Seq[String]]('t', "tickers") valueName "<ticker>,<ticker>..."  action { (value, config) =>
         config.copy(tickers = value)
       } text "tickers"
+      opt[Seq[String]]('j', "jars") valueName "<jar1>,<jar2>..."  action { (value, config) =>
+        config.copy(jars = value)
+      } text "jars"
     }
     parser.parse(args, StockCorrelationConfig())
   }
@@ -31,7 +43,8 @@ object StockCorrelation extends Logging {
       None
     } foreach { config =>
       val comparisons = doCorrelations(config)
-      comparisons foreach(println(_))
+      info("Finished processing")
+      comparisons foreach(x => info(x.toString()))
     }
   }
 
@@ -48,10 +61,16 @@ object StockCorrelation extends Logging {
 
   def doCorrelations(config: StockCorrelationConfig): Seq[(String, String, Double)] = {
     val context       = Spark.sparkContext(config.sparkUrl)
+    config.jars.foreach{ jar =>
+      debug(s"Adding JAR $jar")
+      context.addJar(jar)
+    }
     val pairs         = comparisonPairs(config.tickers)
+    debug(s"Comparing: ${pairs.mkString(",")}")
     val all           = context.wholeTextFiles(config.directory)
     val aggregated    = aggregate(all, isNotMeta, dateTickerToPrice)
     val pairsCorr     = pairs map { case(t1, t2) =>
+      debug(s"processing $t1 and $t2")
       val series1 = aggregated.filter(matchesTicker(t1, _)).map(asDateToPrice)
       val series2 = aggregated.filter(matchesTicker(t2, _)).map(asDateToPrice)
       (t1, t2, pearsonCorrelationValue(series1, series2))
