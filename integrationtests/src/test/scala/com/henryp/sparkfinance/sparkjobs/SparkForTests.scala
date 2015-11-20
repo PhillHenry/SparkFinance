11a6256bfc36b73e0643f9864df47af6c9814ae3
PhillHenry <phillhenry@gmail.com>
2015-11-20 15:55:09 +0000

    Added new ticker series.

--- integrationtests/src/test/scala/com/henryp/sparkfinance/sparkjobs/SparkForTests.scala
+++ integrationtests/src/test/scala/com/henryp/sparkfinance/sparkjobs/SparkForTests.scala
@@ -1,15 +1,18 @@
 package com.henryp.sparkfinance.sparkjobs
 
 import com.henryp.sparkfinance.config.Spark
+import com.henryp.sparkfinance.logging.Logging
 import org.apache.spark.rdd.RDD
 
-object SparkForTests {
+object SparkForTests extends Logging {
 
+  info("===================== Starting Spark Context")
   val context       = Spark.sparkContext()
 
   def wholeTextFiles(dataDirectory: String): RDD[(String, String)]  = context.wholeTextFiles(dataDirectory)
 
   def stop(): Unit = {
+    info("===================== Stopping Spark Context")
     context.stop()
   }
 }
--- integrationtests/src/test/scala/com/henryp/sparkfinance/sparkjobs/yahoo/StockSVMIntegrationSpec.scala
+++ integrationtests/src/test/scala/com/henryp/sparkfinance/sparkjobs/yahoo/StockSVMIntegrationSpec.scala
@@ -3,16 +3,16 @@ package com.henryp.sparkfinance.sparkjobs.yahoo
 import com.henryp.sparkfinance.feeds.yahoo._
 import com.henryp.sparkfinance.sparkjobs.SparkForTests.wholeTextFiles
 import com.henryp.sparkfinance.sparkjobs.yahoo.StockSVM.joinByDate
-import com.henryp.sparkfinance.sparkjobs.yahoo.YahooIntegrationFixture.{barclaysTicker, dataDirectory, hsbcTicker}
+import com.henryp.sparkfinance.sparkjobs.yahoo.YahooIntegrationFixture.{barclaysTicker, dataDirectory, gskTicker, hsbcTicker}
 import org.scalatest.{Matchers, WordSpec}
 
 class StockSVMIntegrationSpec extends WordSpec with Matchers {
 
   "different stocks" should {
     "be converted to a sequence keyed on date" in {
-      val dateToFeatures = joinByDate(barclaysTicker, Seq(hsbcTicker), wholeTextFiles(dataDirectory), dayTickerToPrice)
+      val dateToFeatures = joinByDate(barclaysTicker, Seq(hsbcTicker, gskTicker), wholeTextFiles(dataDirectory), dayTickerToPrice)
       dateToFeatures.count() should be > 0L
-      dateToFeatures.first()._2 should have size 2
+      dateToFeatures.first()._2 should have size 3
     }
   }
 
